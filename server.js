e5b03ac675da41e455833bddfa88203a0097a1be
khoaHyh <khoahuynh.toronto@gmail.com>
2021-03-02 18:52:20 -0500

    Feat: Set up a Template Engine
    Template engine replaces variables in a template file with actual values which can be supplied by your server. Then it transforms the template into a static HTML file that is sent to the client.

--- server.js
+++ server.js
@@ -3,8 +3,16 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
-
 const app = express();
+
+// Implement a Root-Level Request Logger Middleware
+app.use((req, res, next) => {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+});
+
+// template engine lets us use static template files in our app
+app.set('view engine', 'pug');
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -12,10 +20,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-  res.render('Load your view here');
+    // set path to views/pug directory
+    res.render('pug');
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+    console.log('Listening on port ' + PORT);
 });
