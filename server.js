9b5700e1fd73277b994633f95226f214278c6a1a
khoaHyh <khoahuynh.toronto@gmail.com>
2021-03-02 19:56:17 -0500

    Serialization of a User Object

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const app = express();
 const session = require('express-session');
 const passport = require('passport');
+const ObjectID = require('mongodb').ObjectID;
 
 // Implement a Root-Level Request Logger Middleware
 app.use((req, res, next) => {
@@ -13,6 +14,13 @@ app.use((req, res, next) => {
     next();
 });
 
+// template engine lets us use static template files in our app
+app.set('view engine', 'pug');
+
+fccTesting(app); //For FCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -24,18 +32,22 @@ app.use(session({
   cookie: { secure: false }
 }));
 
-// template engine lets us use static template files in our app
-app.set('view engine', 'pug');
-
-fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.route('/').get((req, res) => {
     res.render('pug');
     // set path to views/pug directory and pass variables
     res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'});
+});
+
+// Convert object contents into a key
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+// Convert key into original object
+passport.deserializeUser((id, done) => {
+  //myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+    done(null, null);
+  //});
 });
 
 const PORT = process.env.PORT || 3000;
