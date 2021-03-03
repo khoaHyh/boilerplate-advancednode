814929daae7e7b3db2077725f75e17e7c86616d3
khoaHyh <khoahuynh.toronto@gmail.com>
2021-03-02 20:34:29 -0500

    Implement the Serialization of a Passport User

--- server.js
+++ server.js
@@ -32,22 +32,34 @@ app.use(session({
   cookie: { secure: false }
 }));
 
-app.route('/').get((req, res) => {
-    res.render('pug');
-    // set path to views/pug directory and pass variables
-    res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'});
-});
+// Connect to our db and start listening for requests
+myDB(async client => {
+    const myDataBase = await client.db('database').collection('users');
 
-// Convert object contents into a key
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
+    // Be sure to change the title
+    app.route('/').get((req, res) => {
+    //Change the response to render the Pug template
+        res.render('pug', {
+            title: 'Connected to Database',
+            message: 'Please login'
+        });
+    });
 
-// Convert key into original object
-passport.deserializeUser((id, done) => {
-  //myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-    done(null, null);
-  //});
+    // Convert object contents into a key
+    passport.serializeUser((user, done) => {
+        done(null, user._id);
+    });
+
+    // Convert key into original object
+    passport.deserializeUser((id, done) => {
+        myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+            done(null, null);
+        });
+    });
+}).catch(e => {
+    app.route('/').get((req, res) => {
+        res.render('pug', { title: e, message: 'Unable to login' });
+    });
 });
 
 const PORT = process.env.PORT || 3000;
