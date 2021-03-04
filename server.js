88d380c52cde7c260c0d17c132a79bec6ea23ddc
khoaHyh <khoahuynh.toronto@gmail.com>
2021-03-03 21:40:00 -0500

    Authentication with Socket.IO

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ const routes = require('./routes');
 const auth = require('./auth');
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const passportSocketIo = require('passport.socketio');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
 
 // Implement a Root-Level Request Logger Middleware
 app.use((req, res, next) => {
@@ -29,11 +34,27 @@ app.use(passport.session());
 
 // Set up our express app to use session
 app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  cookie: { secure: false }
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+    key: 'express.sid',
+    store: store
 }));
+
+// Parse and decode the cookie that contains the passport session then deserialize
+// to obtain user object
+io.use(
+    passportSocketIo.authorize({
+        cookieParser: cookieParser,
+        key: 'express.sid',
+        secret: process.env.SESSION_SECRET,
+        store: store,
+        success: onAuthorizeSuccess,
+        fail: onAuthorizeFail
+    })
+);
+
 
 // Connect to our db and start listening for requests
 myDB(async client => {
@@ -41,15 +62,15 @@ myDB(async client => {
 
     routes(app, myDataBase);
     auth(app, myDataBase);
-
+   
     // Keep track of users
     let currentUsers = 0;
     // Listen for connections to our server
     io.on('connection', socket => {
         ++currentUsers;
         io.emit('user count', currentUsers);
-        console.log('A user has connected');
-
+        console.log('user ' + socket.request.user.name + ' connected');
+            
         // Listen for disconnections from our server
         socket.on('disconnect', () => {
             --currentUsers;
@@ -62,6 +83,17 @@ myDB(async client => {
         res.render('pug', { title: e, message: 'Unable to login' });
     });
 });
+
+const onAuthorizeSuccess = (data, accept) => {
+    console.log('successful connection to socket.io');
+    accept(null, true);
+}
+
+const onAuthorizeFail = (data, message, error, accept) => {
+    if (error) throw new Error(message);
+    console.log('failed connection to socket.io:', message);
+    accept(null, false);
+}
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
